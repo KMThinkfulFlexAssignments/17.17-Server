1f58f756221ff7a9f095b049a5c44db3fd09b93b
Kevin Moore <moore.kevin.p@gmail.com>
2020-03-26 10:31:40 -0400

    updated bookmarks-router to support PATCH request

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -100,6 +100,27 @@ bookmarksRouter
         res.status(204).end();
       })
       .catch(next);
+  })
+  .patch(bodyParser, (req, res, next) => {
+    const { title, url, rating, description } = req.body;
+    const bookmarkToUpdate = { title, url, rating, description };
+
+    const numberOfValues = Object.values(bookmarkToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: 'Request body must contain either \'title\', \'url\', \'rating\', or \'description\''
+        }
+      });
+
+    BookmarksService.updateBookmark(
+      req.app.get('db'),
+      req.params.bookmark_id,
+      bookmarkToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end();
+      });
   });
 
 module.exports = bookmarksRouter;
